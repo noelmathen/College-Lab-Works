57bf1bf054adddb2bef71627edadfce50ae8d3c7
Noel Mathen <noelmathen03@gmail.com>
2023-10-14 00:06:42 +0530

    New Commit

--- S5 ML (Machine Learning)/Expt3 - Scatter Plot in R/scatterplot.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-ggplot(iris,aes(x=Petal.Length,y=Petal.Width))+
-  geom_point(aes(shape=factor(Species)))+
-  geom_point(aes(color=factor(Species)))+
-  geom_smooth(method="lm")
-
-
--- S5 ML (Machine Learning)/Expt4 - Histogram in R/histogram.R
+++
@@ -1,5 +0,0 @@
-library(ggplot2)
-data(Titanic)
-Titanic_df <- as.data.frame(Titanic)
-ggplot(Titanic_df, aes(x = Freq)) + geom_histogram(color = "black", fill = "green", bins = 30)
-
