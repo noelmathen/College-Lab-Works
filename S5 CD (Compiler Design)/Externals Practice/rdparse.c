f9697704b761995f2e0359e4011c75df3e24b216
Noel Mathen <noelmathen03@gmail.com>
2024-02-05 16:59:04 +0530

    Learned RD parser

--- S5 CD (Compiler Design)/Externals Practice/rdparse.c
+++ S5 CD (Compiler Design)/Externals Practice/rdparse.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 
 #define SUCCESS 1
-#define FAILED 0
+#define FAILURE 0
 
-char string[64];
-int index = 0;
+char string[100];
+int index =0;
 
-void printAction(const char *production) {
-    printf("%-16s %s\n", string + index, production);
+void printAction(const char *prod){
+    printf("%-16s %s\n", string + index, prod);
 }
 
-int matchTerminal(char terminal) {
-    if (string[index] == terminal) {
+int matchTerminal(int terminal){
+    if(string[index]==terminal){
         index++;
         return SUCCESS;
     }
-    return FAILED;
+    return FAILURE;
 }
 
 int E();
@@ -25,60 +24,62 @@ int T();
 int Tdash();
 int F();
 
-int E() {
-    printAction("E -> T E'");
+int E(){
+    printAction("E -> TE'");
     return T() && Edash();
 }
 
-int Edash() {
-    if (matchTerminal('+')) {
-        printAction("E' -> + T E'");
+int Edash(){
+    if(matchTerminal('+')){
+        printAction("E' -> +TE'");
         return T() && Edash();
-    } else {
-        printAction("E' -> $");
+    }
+    else{
+        printAction("E -> $");
         return SUCCESS;
     }
 }
 
-int T() {
-    printAction("T -> F T'");
+int T(){
+    printAction("T -> FT'");
     return F() && Tdash();
 }
 
-int Tdash() {
-    if (matchTerminal('*')) {
-        printAction("T' -> * F T'");
+int Tdash(){
+    if(matchTerminal('*')){
+        printAction("T' -> *FT'");
         return F() && Tdash();
-    } else {
+    } else{
         printAction("T' -> $");
         return SUCCESS;
     }
 }
 
-int F() {
-    if (matchTerminal('(')) {
-        printAction("F -> ( E )");
-        return E() && matchTerminal(')');
-    } else if (matchTerminal('i')) {
-        printAction("F -> i");
-        return SUCCESS;
+int F(){
+    if(matchTerminal('(')){
+        printAction("F -> (E)");
+        return E();
+    } else{
+        if(matchTerminal('i')){
+            printAction("F -> i");
+            return SUCCESS;
+        }
     }
-    return FAILED;
+    return FAILURE;
 }
 
-int main() {
-    printf("Kindly enter the string: ");
+void main(){
+    printf("Enter the input string: ");
     scanf("%s", string);
+
     printf("\nInput      Action\n");
     printf("--------------------------------\n");
 
-    if (E() && string[index] == '\0') {
-        printf("--------------------------------\n");
-        printf("String parsing success\n");
-        return 0;
-    } else {
-        printf("--------------------------------\n");
-        printf("String parsing failed\n");
-        return 1;
+    if(E() && string[index]=='\0'){
+        printf("------------------------------");
+        printf("\nInput string processed successfully!");
+    } else{
+        printf("------------------------------");
+        printf("\nInput string processed not successfully!");
     }
 }
