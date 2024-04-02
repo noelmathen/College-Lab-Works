d8038409d225d88c39813ae3912c7757e5ae61f2
Noel Mathen <noelmathen03@gmail.com>
2024-04-02 10:39:54 +0530

    AO* updated

--- S6 AI (Artificial Intelligence)/Expt7 - AO_star/AO_star.py
+++ S6 AI (Artificial Intelligence)/Expt7 - AO_star/AO_star.py
@@ -1,73 +1,113 @@
-#Q7)AO* Algorithm
+def AOStar(n):
+    global finalPath
+    print("Expanding Node:",n)
+    and_nodes = []
+    or_nodes =[]
+    if(n in allNodes):
+        if 'AND' in allNodes[n]:
+            and_nodes = allNodes[n]['AND']
+        if 'OR' in allNodes[n]:
+            or_nodes = allNodes[n]['OR']
+    if len(and_nodes)==0 and len(or_nodes)==0:
+        return
+    
+    solvable = False
+    marked ={}
+    
+    while not solvable:
+        if len(marked)==len(and_nodes)+len(or_nodes):
+            #print(len(marked))
+            min_cost_least,min_cost_group_least = least_cost_group(and_nodes,or_nodes,{})
+            solvable = True
+            change_heuristic(n,min_cost_least)
+            optimal_child_group[n] = min_cost_group_least
+            continue
+        min_cost,min_cost_group = least_cost_group(and_nodes,or_nodes,marked)
+        is_expanded = False
+        if len(min_cost_group)>1:
+            if(min_cost_group[0] in allNodes):
+                is_expanded = True
+                AOStar(min_cost_group[0])
+            if(min_cost_group[1] in allNodes):
+                is_expanded = True
+                AOStar(min_cost_group[1])
+        else:
+            if(min_cost_group in allNodes):
+                is_expanded = True
+                AOStar(min_cost_group)
+        if is_expanded:
+            min_cost_verify, min_cost_group_verify = least_cost_group(and_nodes, or_nodes, {})
+            if min_cost_group == min_cost_group_verify:
+                solvable = True
+                change_heuristic(n, min_cost_verify)
+                optimal_child_group[n] = min_cost_group
+        else:
+            solvable = True
+            change_heuristic(n, min_cost)
+            optimal_child_group[n] = min_cost_group
+        marked[min_cost_group]=1
+    return heuristic(n)
 
-# Cost to find the AND and OR path
-def Cost(H, condition, weight = 1):
-	cost = {}
-	if 'AND' in condition:
-		AND_nodes = condition['AND']
-		Path_A = ' AND '.join(AND_nodes)
-		PathA = sum(H[node]+weight for node in AND_nodes)
-		cost[Path_A] = PathA
+def least_cost_group(and_nodes, or_nodes, marked):
+    node_wise_cost = {}
+    for node_pair in and_nodes:
+        if not node_pair[0] + node_pair[1] in marked:
+            cost = 0
+            cost = cost + heuristic(node_pair[0]) + heuristic(node_pair[1]) + 2
+            node_wise_cost[node_pair[0] + node_pair[1]] = cost
+    for node in or_nodes:
+        if not node in marked:
+            cost = 0
+            cost = cost + heuristic(node) + 1
+            node_wise_cost[node] = cost
+    min_cost = 999999
+    min_cost_group = None
+    for costKey in node_wise_cost:
+        if node_wise_cost[costKey] < min_cost:
+            min_cost = node_wise_cost[costKey]
+            min_cost_group = costKey
+    return [min_cost, min_cost_group]
 
-	if 'OR' in condition:
-		OR_nodes = condition['OR']
-		Path_B =' OR '.join(OR_nodes)
-		PathB = min(H[node]+weight for node in OR_nodes)
-		cost[Path_B] = PathB
-	return cost
+def heuristic(n):
+    return H_dist[n]
 
-# Update the cost
-def update_cost(H, Conditions, weight=1):
-	Main_nodes = list(Conditions.keys())
-	Main_nodes.reverse()
-	least_cost= {}
-	for key in Main_nodes:
-		condition = Conditions[key]
-		c = Cost(H, condition, weight) 
-		H[key] = min(c.values())
-		least_cost[key] = Cost(H, condition, weight)		 
-	return least_cost
+def change_heuristic(n, cost):
+    H_dist[n] = cost
+    return
 
-# Print the shortest path
-def shortest_path(Start,Updated_cost, H):
-	Path = Start
-	if Start in Updated_cost.keys():
-		Min_cost = min(Updated_cost[Start].values())
-		key = list(Updated_cost[Start].keys())
-		values = list(Updated_cost[Start].values())
-		Index = values.index(Min_cost)
-		
-		# FIND MINIMIMUM PATH KEY
-		Next = key[Index].split()
-		# ADD TO PATH FOR OR PATH
-		if len(Next) == 1:
+def print_path(node):
+        print(optimal_child_group[node], end="")
+        node = optimal_child_group[node]
+        if len(node) > 1:
+            if node[0] in optimal_child_group:
+                print(" -> ", end="")
+                print_path(node[0])
+            if node[1] in optimal_child_group:
+                print(" -> ", end="")
+                print_path(node[1])
 
-			Start =Next[0]
-			Path += '<--' +shortest_path(Start, Updated_cost, H)
-		# ADD TO PATH FOR AND PATH
-		else:
-			Path +='<--('+key[Index]+') '
+        else:
+            if node in optimal_child_group:
+                print(" -> ", end="")
+                print_path(node)
+            
+H_dist = eval(input("Enter the key-value corresponding to parent and its heuristics : "))
 
-			Start = Next[0]
-			Path += '[' +shortest_path(Start, Updated_cost, H) + ' + '
+allNodes = eval(input("\nEnter your graph : "))
 
-			Start = Next[-1]
-			Path += shortest_path(Start, Updated_cost, H) + ']'
+startnode = input("\nEnter the start node : ")
+goalnode = input("\nEnter the goal node : ")
+print("\n")
 
-	return Path
-		
-H = {'A': -1, 'B': 5, 'C': 2, 'D': 4, 'E': 7, 'F': 9, 'G': 3, 'H': 0, 'I':0, 'J':0}
+optimal_child_group = {}
+optimal_cost = AOStar(startnode)
 
-Conditions = {
-'A': {'OR': ['B'], 'AND': ['C', 'D']},
-'B': {'OR': ['E', 'F']},
-'C': {'OR': ['G'], 'AND': ['H', 'I']},
-'D': {'OR': ['J']}
-}
+path = []
+path = optimal_child_group.values()
 
-# weight
-weight = 1
-# Updated cost
-Updated_cost = update_cost(H, Conditions, weight=1)
-
-print('Shortest Path :\n',shortest_path('A', Updated_cost,H))
+if not goalnode in path:
+  print("\nPath from ", startnode, " to ", goalnode, "not present")
+else:
+  print("\nNodes which gives optimal cost from the start node ", startnode, " are : ")
+  print_path(startnode)
+  print('\n\nOptimal cost is ', optimal_cost)
