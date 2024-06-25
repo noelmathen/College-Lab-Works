79b7d5ffcce023189ce8aa18e5419d3d2328f331
Noel Mathen <noelmathen03@gmail.com>
2024-06-25 13:06:02 +0530

    waterjug ese practice done

---
+++ S6 AI (Artificial Intelligence)/Practice/Externals Practice/waterJugExt.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+def water_jug(x, y):
+    if (x, y) in visited:
+        return False
+    
+    print(x, y)
+    visited[(x, y)] = True
+    
+    if x==aim or y==aim:
+        return True
+    
+    return(
+        water_jug(x, 0) or
+        water_jug(0, y) or
+        water_jug(jug1, y) or
+        water_jug(x, jug2) or
+        water_jug(x+min(y, jug1-x), y-min(y, jug1-x)) or
+        water_jug(x-min(x, jug2-y), y+min(x, jug2-y))
+    )
+
+
+jug1 = int(input("Enter capacity of jug1: "))
+jug2 = int(input("Enter capacity of jug2: "))
+aim = int(input("Enter the desired quantity: "))
+
+visited = defaultdict(bool)
+
+if not water_jug(0, 0):
+    print("Solution not found")
