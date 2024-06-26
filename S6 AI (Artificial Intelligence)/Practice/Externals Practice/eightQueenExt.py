0b12b23e2544bd4335eb6afd9425b3d430b3043f
Noel Mathen <noelmathen03@gmail.com>
2024-06-26 06:24:11 +0530

    8queenns final

--- S6 AI (Artificial Intelligence)/Practice/Externals Practice/eightQueenExt.py
+++ S6 AI (Artificial Intelligence)/Practice/Externals Practice/eightQueenExt.py
@@ -1,40 +1,37 @@
-n = int(input("Enter the number of queens: "))
-board = [[0]*n for _ in range(n)]
-
 def is_safe(row, col):
-    
-    for i in range(col):
+    for i in range(row):
         if board[row][i]==1:
             return False
     
-    
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+    for i, j in zip(range(col, -1, -1), range(row, -1, -1)):
         if board[i][j]==1:
             return False
-    
-    
-    for i, j in zip(range(row, n), range(col, -1, -1)):
+        
+    for i, j in zip(range(row, n), range(row, -1, -1)):
         if board[i][j]==1:
             return False
     
     return True
-    
+        
 
 def solve_n_queens(col):
     if col>=n:
         return True
-    
-    for row in range(n):    
-        if is_safe(row, col):
-            board[row][col]=1
+
+    for i in range(n):
+        if is_safe(i, col):
+            board[i][col]=1
             if solve_n_queens(col+1):
                 return True
-            board[row][col]=0
+            board[i][col]=0
     return False
 
+
+n = int(input("Enter the number of queens: "))
+board = [[0]*n for _ in range(n)]
 
 if solve_n_queens(0):
     for row in board:
         print(row)
 else:
-    print("Solution not found")
+    print("Solution not found!")
