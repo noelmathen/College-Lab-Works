4fd0e057e89ebde33046212b403c1ba8623d3833
Noel Mathen <noelmathen03@gmail.com>
2024-06-26 07:53:25 +0530

    tictactoe final

--- S6 AI (Artificial Intelligence)/Practice/Externals Practice/tictactoeExt.py
+++ S6 AI (Artificial Intelligence)/Practice/Externals Practice/tictactoeExt.py
@@ -1,47 +1,46 @@
+board = [' ']*10
 running, draw, win = 0, -1, 1
 game = running
 player = 1
-board = [' '] * 10
 
-def draw_board():
+def DrawBoard():
     print("\n".join([
         f" {board[1]} | {board[2]} | {board[3]}",
         f"---|---|---",
         f" {board[4]} | {board[5]} | {board[6]}",
         f"---|---|---",
         f" {board[7]} | {board[8]} | {board[9]}",
-    ]))
+]))
+    
 
-def check_win():
-    win = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+def checkWin():
+    win = [(1,2,3), (4,5,6), (7,8,9), (1,4,7), (2,5,8), (3,6,9), (1,5,9), (3,5,7)]
     for a, b, c in win:
         if board[a] == board[b] == board[c] != ' ':
             return win
-    return draw if ' ' not in board else running
+    return draw if ' ' not in board[1:] else running 
 
-
-draw_board()
+DrawBoard()
 while game==running:
-    print(f"\nPlayer {player}'s move ({'X' if player==1 else 'O'})")
-    choice = int(input('Enter a position[1-9]: '))
-    
+    print(f"Player {player}'s move ({'X' if player==1 else 'O'})")
+    choice = int(input("Enter a board position: "))
     if choice>9 or choice<1:
-        print("Enter a valid choice u dumbass!")
-        draw_board()
+        print("Enter a valid choice")
+        DrawBoard()
         continue
     
     if board[choice]==' ':
         board[choice] = 'X' if player==1 else 'O'
         player = 1 if player==2 else 2
-        draw_board()
-        game=check_win()
-    
+        DrawBoard()
+        game = checkWin()
+        
     else:
-        print('Position already occuppied! Try again')
-        draw_board()
+        print("Position already occupied! trry again")
+        DrawBoard()
         
 if game==draw:
-    print("DRAW!")
+    print("Draw")
 else:
-    player = 1 if player==2 else 2
-    print(f"Player {player} won!")
+    print(f"Player {1 if player==2 else 1} won")
+
