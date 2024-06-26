05560bf6d8d170668235f7053a27b08c1336cf9a
Noel Mathen <noelmathen03@gmail.com>
2024-06-26 07:35:27 +0530

    alphabeta final

--- S6 AI (Artificial Intelligence)/Practice/Externals Practice/alphabetaExt.py
+++ S6 AI (Artificial Intelligence)/Practice/Externals Practice/alphabetaExt.py
@@ -3,16 +3,17 @@ MIN, MAX = -1000, 1000
 def minimax(depth, nodeIndex, isMax, values, alpha, beta):
     if depth==3:
         return values[nodeIndex]
-    
+
     if isMax:
         best = MIN
         for i in range(2):
             value = minimax(depth+1, nodeIndex*2+i, False, values, alpha, beta)
             best = max(best, value)
             alpha = max(alpha, best)
-            if alpha >= beta:
+            if alpha>=beta:
                 break
         return best
+    
     else:
         best = MAX
         for i in range(2):
@@ -22,7 +23,7 @@ def minimax(depth, nodeIndex, isMax, values, alpha, beta):
             if alpha>=beta:
                 break
         return best
-
-values =  [2, 3, 5, 9, 0, 1, 7, 5]
-print(f"Values: {values}")
-print(f"Optimal value: {minimax(0, 0, True, values, MIN, MAX)}")
+    
+values = [3, 5, 6, 9, 1, 2, 0, -1]
+print(f"Values are: {values}")
+print(f"Best node is: {minimax(0, 0, True, values, MIN, MAX)}")
